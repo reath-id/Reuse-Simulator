ac3fd2a9a9f5c2e7d3523e09c1402a4eb34d0aec
Vvaridus <59836794+Vvaridus@users.noreply.github.com>
2021-03-11 14:23:09 +0000

    Design commit one
    initial design commit

---
+++ ReathUIv0.2/Graphs.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ReathUIv0._1
+{
+    /// <summary>
+    /// Interaction logic for Graphs.xaml
+    /// </summary>
+    public partial class Graphs : Window
+    {
+        public Graphs()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInput_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnGraphs_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnData_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSettings_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
